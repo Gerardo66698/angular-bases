6889ba5ab619d1936cc87833c71b3cf2689471c6
LGeraGM <luisgerardogm98@gmail.com>
2024-05-15 14:30:22 -0600

    Fin de seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-BASES';
+  public title: string = '02-BASES: Mi Primera App de Angular';
+  // public counter: number = 10;
+
+  // increaseBy(value:string):void{
+    // if(value=='+'){
+    //   this.counter+=1;
+    // }else{
+    //   this.counter-=1;
+    // }
+  //   value=='+'? this.counter+=1 : this.counter-=1;
+  // }
+
+  // increaseBy(value:number):void{
+  //   this.counter+=value;
+  // }
+
+  // resetCounter():void{
+  //   this.counter=10;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './counter/counter.module';
+import { HerosModule } from './heros/heros.module';
+
 import { AppComponent } from './app.component';
+// import { HeroComponent } from './heros/hero/hero.component';
+// import { ListComponent } from './heros/list/list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // HeroComponent,
+    // ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HerosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
